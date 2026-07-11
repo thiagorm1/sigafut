974c21eb2eace0c46d7e75f7f7e9a9577665e22f
SigaFut Admin <admin@sigafut.com>
2026-07-11 00:40:34 -0300

    feat: enable local model script to stream stats to project database

--- sigafut-api/src/routes/matchRoutes.js
+++ sigafut-api/src/routes/matchRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getMatchEvents, getMatchStats } = require('../controllers/matchController');
+const { getMatchEvents, getMatchStats, createMatchEvent } = require('../controllers/matchController');
 
 router.get('/:matchId/events', getMatchEvents);
 router.get('/:matchId/stats', getMatchStats);
+router.post('/:matchId/events', createMatchEvent);
 
 module.exports = router;
